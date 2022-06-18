6d4c72f78f865380c1ace602cf2f180d3eb17d59
Samuel Obi <samobi588@gmail.com>
2022-06-18 14:37:43 -0700

    codewars-8kyu

---
+++ convertToString.js
@@ -0,0 +1,11 @@
+// We need a function that can transform a number into a string.
+
+function numberToString(num) {
+    return num.toString()
+}
+
+// or 
+
+function numberToString(num) {
+    return String(num)
+}
