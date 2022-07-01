c3ccf7978c1e04dfc9fca706fc1309a5c2ae2830
Samuel Obi <samobi588@gmail.com>
2022-07-01 14:21:45 -0700

    codewars-8kyu

---
+++ checkForFactor.js
@@ -0,0 +1,11 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+
+function checkForFactor (base, factor) {
+    return base % factor === 0
+}
