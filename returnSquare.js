c873c9cdc5a09d5259a773c9872286623bfa4fb8
Samuel Obi <samobi588@gmail.com>
2022-07-06 17:49:05 -0700

    codewars-8kyu

---
+++ returnSquare.js
@@ -0,0 +1,10 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+const square = num => Math.pow(num,2) //using arrow function
+
+//or
+
+function square(num){
+    return Math.pow(num,2)
+}
