8f781f19a32091b0b430db70a7f9e38d4859faa1
Samuel Obi <samobi588@gmail.com>
2022-07-08 15:21:36 -0700

    codewars-8kyu

---
+++ sumStrings.js
@@ -0,0 +1,11 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+
+
+function sumStr(a,b) {
+    let sum = Number(a) + Number(b)
+    return `${sum}`
+}
